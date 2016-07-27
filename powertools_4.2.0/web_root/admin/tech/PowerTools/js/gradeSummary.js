bb0c08f4a9c4b6d0aedf843cf3b0b96f1f6da71d
Timothy Gates <Timothy Gates>
2016-07-27 01:31:05 -0700

    further work on tree

--- powertools_4.2.0/web_root/admin/tech/PowerTools/js/gradeSummary.js
+++ powertools_4.2.0/web_root/admin/tech/PowerTools/js/gradeSummary.js
@@ -9,11 +9,21 @@ var GradeNode = function(grade){
 	this.value = grade;
 	this.x = 0;
 	this.y = 0;
+	this.visible = false;
 }
 GradeNode.prototype.addChild = function(grade){
 	this.children.push(new GradeNode(grade));
 	this.children[this.children.length -1].parent = this;
 }
-GradeNode.prototype.setPosition = function(){
-	
+GradeNode.prototype.setPosition = function(x,y){
+	this.x = x;
+	this.y = y;
+}
+GradeNode.prototype.buildTreePositions = function(head){
+	if(this == head){
+		
+	}
+	else{
+		this
+	}
 }
