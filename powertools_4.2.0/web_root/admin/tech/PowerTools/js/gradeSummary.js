f1fea551172a95c10639973fed72494328adae64
Timothy-Gates <timothy.gates@powerschool.com>
2016-08-02 16:23:55 -0700

    added grade summary

--- powertools_4.2.0/web_root/admin/tech/PowerTools/js/gradeSummary.js
+++ powertools_4.2.0/web_root/admin/tech/PowerTools/js/gradeSummary.js
@@ -9,11 +9,67 @@ var GradeNode = function(grade){
 	this.value = grade;
 	this.x = 0;
 	this.y = 0;
+	this.preX = 0;
+	this.prevY = 0;
+	this.visibility= false;
+	this.tree = [];
+	this.height = 0;
 }
-GradeNode.prototype.addChild = function(grade){
-	this.children.push(new GradeNode(grade));
-	this.children[this.children.length -1].parent = this;
+GradeNode.prototype.addChild = function(grade, parent){
+	if(this.value == parent){
+		this.children.push(new GradeNode(grade));
+		this.children[this.children.length -1].parent = this;
+		return;
+	}
+	for(var i = 0; i< this.children.length; i++){
+		this.children[i].addChild(grade, parent);
+	}
 }
-GradeNode.prototype.setPosition = function(){
-	
+GradeNode.prototype.makeVisible = function(head){
+	for(var i = 0; i < this.children.length; i++){
+		this.children[i].visibility = true;
+	}
+	head.getDimentions(head);
+}
+GradeNode.prototype.getDimentions = function(head){
+	if(head.tree.length < head.tree.height - 1){
+		head.tree.length.push(1);
+	}
+	else{
+		head.tree[height-1] += 1;
+	}
+	for(var i = 0; i < this.children.length; i++){
+		if(this.children[i].visibility == true){
+			head.height++;
+			this.children[i].setPosition(head);
+			head.height--;
+		}
+		else{
+			break;
+		}
+	}
+	if(this == head){
+		this.setPosition(head);
+	}
+}
+GradeNode.prototype.setPosition = function(head){
+	for(var i = 0; i < this.children.length; i++){
+		if(this.children[i].visibility == true){
+			head.height++;
+			this.children[i].setPosition(head);
+			head.height--;
+		}
+		else{
+			break;
+		}
+	}
+	this.prevX = this.x;
+	this.prevY = this.y;
+	this.x = (this.tree.length - 1 - this.height) * 100;
+	if(this.children.length > 0){
+		
+	}
+	else{
+		
+	}
 }
